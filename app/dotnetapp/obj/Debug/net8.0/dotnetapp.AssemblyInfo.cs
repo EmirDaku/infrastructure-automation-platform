f6a77914bfcd2143ea5ee2bdea496f29d2d589e2
EmirDaku <emirdaku82@gmail.com>
2026-02-08 13:25:52 +0100

    workflows

--- app/dotnetapp/obj/Debug/net8.0/dotnetapp.AssemblyInfo.cs
+++ app/dotnetapp/obj/Debug/net8.0/dotnetapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7153869985a670a7c775387b239fcfe4e3e72bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17aba0fca33d407eb34565ef5e9df0fc0b2b1769")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
